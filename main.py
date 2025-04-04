0086dcd5bcba544f7df1d25c5622e9db34e498bd
IMDelewer <randomezeryt@gmail.com>
2025-04-04 15:15:56 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -95,4 +95,4 @@ def stop_stream(stream_key: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
